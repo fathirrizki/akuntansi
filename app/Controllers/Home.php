38f8b3c5de4d919ad0fa0a5ccad7a37b237800cc
Fathir Rizki <fathirrizki78@gmail.com>
2025-06-13 21:05:56 +0700

    add login & register views

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,11 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('auth/login');
     }
+
+    public function register(): string //SESUAI PLAYLIST
+    { //SESUAI PLAYLIST
+        return view('auth/register'); //SESUAI PLAYLIST
+    } //SESUAI PLAYLIST
 }
